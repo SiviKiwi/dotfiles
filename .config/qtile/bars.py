c283c57f63f0b4b2a8145578f002031f2980e7a0
SiviKiwi <lauvdalsivert@gmail.com>
2021-07-07 21:08:03 +0200

    qtile: initial commit.

---
+++ .config/qtile/bars.py
@@ -0,0 +1,22 @@
+from libqtile import bar, widget
+
+main_bar = bar.Bar(
+        [
+            widget.GroupBox(),
+            widget.Prompt(),
+            #widget.WindowName(),
+            widget.Spacer(
+                lenght=bar.STRETCH
+                ),
+            widget.PulseVolume(),
+            widget.CheckUpdates(
+                display_format='Updates: {updates}',
+                distro='Arch',
+                colour_have_updates='990000',
+                color_no_updates='007300',
+                update_interval=1800,
+                execute='alacritty -e sudo pacman -Syu'
+                ),
+            widget.Systray(),
+            widget.Clock(format='%d-%m-%Y %a %H:%M'),
+        ], 24)

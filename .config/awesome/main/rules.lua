c57ba54f0c13d8f75dc635b59c47ea3200663efd
SiviKiwi <lauvdalsivert@gmail.com>
2021-07-07 21:06:36 +0200

    Move focus to tag when moving client to that tag.

--- .config/awesome/main/rules.lua
+++ .config/awesome/main/rules.lua
@@ -72,8 +72,8 @@ function _M.get(clientkeys, clientbuttons)
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    { rule = { class = "Firefox" },
+      properties = { screen = 1, tag = "5" } },
 
   }
 

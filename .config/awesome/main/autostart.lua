ebacdffcdefa8339d03b26a6196d66d1207d3f6c
SiviKiwi <lauvdalsivert@gmail.com>
2021-10-11 14:03:34 +0200

    Removed awesome configs.

--- .config/awesome/main/autostart.lua
+++
@@ -1,9 +0,0 @@
--- Standard awesome library
-local awful = require("awful")
-
-
--- Start compositor
-awful.spawn.with_shell("picom")
-
--- Set wallpaper
-awful.spawn.with_shell("nitrogen --restore")
--- .config/awesome/main/user-variables.lua
+++
@@ -1,9 +0,0 @@
-local _M = {
-    -- This is used later as the default terminal and editor to run.
-    terminal = "alacritty",
-
--- Default modkey.
-    modkey = "Mod4",
-}
-
-return _M

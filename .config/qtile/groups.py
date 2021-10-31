3ccc9a59d4f60f21c22f0f47da5b061a8932e0a4
SiviKiwi <lauvdalsivert@gmail.com>
2021-10-31 21:15:25 +0100

    Removed radio-group.

--- .config/qtile/groups.py
+++ .config/qtile/groups.py
@@ -1,6 +1,4 @@
 from libqtile.config import Group, Match
 from libqtile import layout
 
-groups = [Group(i) for i in "12345678"]
-
-groups.append(Group('9', label='radio', layout='floating', matches=[Match(title='radio')]))
+groups = [Group(i) for i in "123456789"]

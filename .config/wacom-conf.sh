6f7a282dbcce30ec4733b40358a50a5f5404be7b
SiviKiwi <lauvdalsivert@gmail.com>
2021-10-31 14:24:28 +0100

    Intitial commmit

---
+++ .config/wacom-conf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+for i in $(seq 10); do
+    if xsetwacom list devices | grep -q Wacom; then
+        break
+    fi
+    sleep 1
+done
+
+list=$(xsetwacom list devices)
+stylus=$(echo "${list}" | awk '/stylus/{print $9}')
+
+if [ -z "${stylus}" ]; then
+    exit 0
+fi
+
+# configure the buttons on ${stylus} with your xsetwacom commands...
+#xsetwacom set "${stylus}" Button 2 11
+
+deceleration=$(xinput list-props "${stylus}" | grep "Constant Deceleration" | awk -F'[()]' '{print $2}')
+velocity=$(xinput list-props "${stylus}" | grep "Velocity Scaling" | awk -F'[()]' '{print $2}')
+
+xsetwacom set "${stylus}" Area 0 0 24000 15000
+xsetwacom set "${stylus}" Mode Relative
+xsetwacom set "${stylus}" CursorProximity 20
+xsetwacom set "${stylus}" Rotate half
+xinput set-prop "${stylus}" "${deceleration}" 2.5
+xinput set-prop "${stylus}" "${velocity}" 10

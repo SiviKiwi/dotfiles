6d770bf514dd56b9810d9a5739a40f3bf80f5025
SiviKiwi <lauvdalsivert@gmail.com>
2021-12-06 07:04:45 +0100

    Removed border from float layout.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -13,9 +13,9 @@ from screens import screens
 
 
 layouts = [
-        layout.MonadTall(border_width=1, single_border_width=0),
+        layout.MonadTall(margin=8, single_margin=16, border_width=1, single_border_width=0),
         #layout.Columns(border_focus_stack='#d75f5f'),
-        layout.Floating(),
+        layout.Floating(border_width=0),
 
 ]
 

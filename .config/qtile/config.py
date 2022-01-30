66f709b9af89f46d4bfcda9eafd9dd1801d67daf
SiviKiwi <lauvdalsivert@gmail.com>
2022-01-30 19:55:12 +0100

    Removed riot client and league client from config.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -51,8 +51,6 @@ floating_layout = layout.Floating(float_rules=[
     Match(wm_class='battle.net.exe'), # Battle.net
     Match(wm_class='display'), # Imagemagic display
     Match(title='Åpne bilde'), # Xournalpp choose image
-    Match(wm_class='riotclientux.exe'), # Riot client
-    Match(wm_class='leagueclientux.exe'), # League client
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"

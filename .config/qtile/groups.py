2d53b960e2ba4e8461ed9b8c19b5ee189f2c9888
SiviKiwi <lauvdalsivert@gmail.com>
2022-04-19 03:09:33 +0200

    Add qtile to startup and start it in group 8.

--- .config/qtile/groups.py
+++ .config/qtile/groups.py
@@ -1,6 +1,7 @@
 from libqtile.config import Group, Match
 from libqtile import layout
 
-groups = [Group(i) for i in "12345678"]
+groups = [Group(i) for i in "1234567"]
 
+groups.append(Group('8', label='8', matches=[Match(wm_class='qbittorrent')]))
 groups.append(Group('9', label='radio', layout='floating', matches=[Match(title='radio')]))
--- .config/qtile/startup.sh
+++ .config/qtile/startup.sh
@@ -4,5 +4,6 @@ exec picom &
 exec nitrogen --restore &
 exec alacritty -t radio -e /home/sivert/.bin/radio.sh &
 
+exec qbittorrent &
 exec thunderbird &
 exec discord &

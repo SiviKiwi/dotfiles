c296341afaf5a14a94ef48cf2a1ab8b72db08e06
Sivi Kiwi <lauvdalsivert@gmail.com>
2022-11-02 05:07:10 +0100

    init commit exwm

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -10,6 +10,32 @@
 
 (setq calendar-week-start-day 1)
 
+;;; EXWM config
+(require 'exwm)
+(require 'exwm-config)
+
+(setq exwm-workspace-number 10)
+
+(setq exwm-input-global-keys
+      `(
+        ;; Bind "s-r" to exit char-mode and fullscreen mode.
+        ([?\s-q] . exwm-reset)
+        ;; Bind "s-w" to switch workspace interactively.
+        ([?\s-w] . exwm-workspace-switch)
+        ;; Bind "s-0" to "s-9" to switch to a workspace by its index.
+        ,@(mapcar (lambda (i)
+                    `(,(kbd (format "s-%d" i)) .
+                      (lambda ()
+                        (interactive)
+                        (exwm-workspace-switch-create ,i))))
+                  (number-sequence 0 9))
+        ;; Bind "s-r" to launch applications ('M-&' also works if the output
+        ;; buffer does not bother you).
+        ([?\s-r] . (lambda (command)
+                     (interactive (list (read-shell-command "$ ")))
+                     (start-process-shell-command command nil command)))))
+
+;;; Emacs config
 (use-package mediawiki
   :config
   (setq mediawiki-site-alist '(("MotstandenWiki"
@@ -18,6 +44,10 @@
         ""
         nil
         "Forside"))))
+
+(use-package typescript-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode)))
 
 (defun int-bin (s)
   "convert an integer into it's binary representation in string format"
@@ -35,7 +65,3 @@
   (let ((command-output (int-bin (buffer-substring (mark) (point)))))
     (kill-region (mark) (point))
     (insert command-output)))
-
-(use-package typescript-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode)))

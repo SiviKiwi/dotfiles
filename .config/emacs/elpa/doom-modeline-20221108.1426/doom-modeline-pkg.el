7909d01e4b0bea66a0125153ec23c8ce4129f925
SiviKiwi <lauvdalsivert@gmail.com>
2023-06-19 18:57:57 +0200

    Added old emacs config

---
+++ .config/emacs/elpa/doom-modeline-20221108.1426/doom-modeline-pkg.el
@@ -0,0 +1,14 @@
+(define-package "doom-modeline" "20221108.1426" "A minimal and modern mode-line"
+  '((emacs "25.1")
+    (compat "28.1.1.1")
+    (shrink-path "0.2.0"))
+  :commit "28e6a3698569b3e78c2515873550f97f0bc93aef" :authors
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("faces" "mode-line")
+  :url "https://github.com/seagle0128/doom-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .config/emacs/elpa/meow-20221216.42/meow-pkg.el
@@ -0,0 +1,12 @@
+(define-package "meow" "20221216.42" "Yet Another modal editing"
+  '((emacs "27.1"))
+  :commit "46cf7cc9e8447fe9219675903fbd1a547bb21ce4" :authors
+  '(("Shi Tianshu"))
+  :maintainer
+  '("Shi Tianshu")
+  :keywords
+  '("convenience" "modal-editing")
+  :url "https://www.github.com/DogLooksGood/meow")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

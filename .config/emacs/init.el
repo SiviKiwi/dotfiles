4f090c9652ff0e9b59a46ff35fd7b9933171876b
SiviKiwi <lauvdalsivert@gmail.com>
2023-09-04 14:27:00 +0200

    Add babel python

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,9 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-format-latex-options
-   '(:foreground default :background default :scale 10.0 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
-		 ("begin" "$1" "$" "$$" "\\(" "\\["))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
